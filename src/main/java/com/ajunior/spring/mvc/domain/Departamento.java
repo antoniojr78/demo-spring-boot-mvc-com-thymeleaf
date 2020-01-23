c438f8fb0a180a8e6041c84352a4b83d94c93d71
antoniojr <antonio.silva@wikki.com.br>
2020-01-23 14:39:35 -0300

    Finalizing the Department Entity Class.

--- src/main/java/com/ajunior/spring/mvc/domain/Departamento.java
+++ src/main/java/com/ajunior/spring/mvc/domain/Departamento.java
@@ -1,5 +1,26 @@
 package com.ajunior.spring.mvc.domain;
 
+import javax.persistence.*; // import javax.persistence.Entity;
+
+@Entity
+@Table(name = "DEPARTAMENTOS")
 public class Departamento extends AbstractEntity<Long> {
+	
+	// OBS.: The Id will be inherited through AbstractEntity
+	
+	/* or just @Column(nullable = false, unique = true, length = 60)
+	 because in this case the column name is the same as the attribute name */
+	@Column(name = "nome", nullable = false, unique = true, length = 60)
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
 
 }
