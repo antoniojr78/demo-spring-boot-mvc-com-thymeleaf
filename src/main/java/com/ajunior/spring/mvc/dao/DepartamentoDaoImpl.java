afde1a1a381b74c31d187835a7072da74e879054
antoniojr <antonio.silva@wikki.com.br>
2020-01-25 15:45:00 -0300

    Creating Department DAO and Interface.

---
+++ src/main/java/com/ajunior/spring/mvc/dao/DepartamentoDaoImpl.java
@@ -0,0 +1,9 @@
+package com.ajunior.spring.mvc.dao;
+
+import com.ajunior.spring.mvc.domain.Departamento;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DepartamentoDaoImpl extends AbstractDao<Departamento, Long> implements DepartamentoDao {
+
+}
