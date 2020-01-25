d78a7e3be8180af6bc198740edaa605965247670
antoniojr <antonio.silva@wikki.com.br>
2020-01-25 15:44:20 -0300

    Creating Position DAO and Interface.

---
+++ src/main/java/com/ajunior/spring/mvc/dao/CargoDaoImpl.java
@@ -0,0 +1,9 @@
+package com.ajunior.spring.mvc.dao;
+
+import com.ajunior.spring.mvc.domain.Cargo;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CargoDaoImpl extends AbstractDao<Cargo, Long> implements CargoDao {
+
+}
