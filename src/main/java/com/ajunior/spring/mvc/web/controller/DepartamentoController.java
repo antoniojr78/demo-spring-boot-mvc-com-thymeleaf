f6ccd1cf88bab0ddce5ee6d45247f3f39cea8ed2
antoniojr <antonio.silva@wikki.com.br>
2020-01-29 11:15:13 -0300

    Implementing insertion of Departments.

--- src/main/java/com/ajunior/spring/mvc/web/controller/DepartamentoController.java
+++ src/main/java/com/ajunior/spring/mvc/web/controller/DepartamentoController.java
@@ -1,20 +1,33 @@
 package com.ajunior.spring.mvc.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import com.ajunior.spring.mvc.domain.Departamento;
+import com.ajunior.spring.mvc.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 	
+	@Autowired
+	private DepartamentoService service;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Departamento departamento) {
 		return "/departamento/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String listar() {
 		return "/departamento/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Departamento departamento) {
+		service.salvar(departamento);
+		// Obs.: URL set in the sidebar
+		return "redirect:/departamentos/cadastrar";
 	}
 }
