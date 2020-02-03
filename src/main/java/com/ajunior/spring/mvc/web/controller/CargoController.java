7bdf463254fb203a9f2356cc70a9b83b47ce331a
antoniojr <antonio.silva@wikki.com.br>
2020-02-03 16:46:53 -0300

    Job: Controller and View.

--- src/main/java/com/ajunior/spring/mvc/web/controller/CargoController.java
+++ src/main/java/com/ajunior/spring/mvc/web/controller/CargoController.java
@@ -1,20 +1,71 @@
 package com.ajunior.spring.mvc.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.ajunior.spring.mvc.domain.Cargo;
+import com.ajunior.spring.mvc.domain.Departamento;
+import com.ajunior.spring.mvc.service.CargoService;
+import com.ajunior.spring.mvc.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
+	
+	@Autowired
+	private CargoService cargoService;
+	@Autowired
+	private DepartamentoService departamentoService;
 
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 		return "/cargo/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("cargos", cargoService.buscarTodos());
 		return "/cargo/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.salvar(cargo);
+		attr.addFlashAttribute("success", "Cargo inserido com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.buscarPorId(id));
+		return "cargo/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.editar(cargo);
+		attr.addFlashAttribute("success", "Cargo atualizado com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String exlcuir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		if (cargoService.cargoTemFuncionarios(id)) {
+			attr.addFlashAttribute("fail", "Cargo não removido. Possui Funcionários(s) vinculado(s).");
+		} else {
+			cargoService.excluir(id);
+			attr.addFlashAttribute("success", "Cargo excluído com sucesso.");
+		}
+		return "redirect:/cargos/listar";
+	}
+	
+	@ModelAttribute("departamentos")
+	public List<Departamento> listaDeDepartamentos() {
+		return departamentoService.buscarTodos();
 	}
 }
---
+++ src/main/java/com/ajunior/spring/mvc/web/converter/StringToDepartamentoConverter.java
@@ -0,0 +1,26 @@
+package com.ajunior.spring.mvc.web.converter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import com.ajunior.spring.mvc.domain.Departamento;
+import com.ajunior.spring.mvc.service.DepartamentoService;
+
+// @ Component - Transforming into a Bean managed by Spring
+@Component
+public class StringToDepartamentoConverter implements Converter<String, Departamento> {
+	
+	@Autowired
+	private DepartamentoService service;
+
+	@Override
+	public Departamento convert(String text) {
+		if (text.isEmpty()) {
+			return null;
+		}
+		Long id = Long.valueOf(text);
+		return service.buscarPorId(id);
+	}
+
+}
