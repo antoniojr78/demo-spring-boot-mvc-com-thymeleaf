e8ea9ed80335d99f58151856f4adac1cdd529cd2
antoniojr <antonio.silva@wikki.com.br>
2020-02-05 10:05:37 -0300

    Editing and deleting an Employee.

--- src/main/java/com/ajunior/spring/mvc/web/controller/FuncionarioController.java
+++ src/main/java/com/ajunior/spring/mvc/web/controller/FuncionarioController.java
@@ -41,6 +41,26 @@ public class FuncionarioController {
 		return "redirect:/funcionarios/cadastrar";
 	}
 	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("funcionario", funcionarioService.buscarPorId(id));
+		return "/funcionario/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.editar(funcionario);
+		attr.addFlashAttribute("success", "Funcionário atualizado com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+		
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		funcionarioService.excluir(id);
+		attr.addFlashAttribute("success", "Funcionário excluído com sucesso.");
+		return "redirect:/funcionarios/listar";
+	}
+	
 	@ModelAttribute("cargos")
 	public List<Cargo> getCargos() {
 		return cargoService.buscarTodos();
